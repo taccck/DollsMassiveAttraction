765a7ac8eadfc5b3ae7b2b4e0664fa77e5795fef
Sam <sam64114208@gmail.com>
2022-04-30 13:48:53 +0200

    Options for gravity

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -1,42 +1,55 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+public enum GravityType { Closest, All };
 
 public class Gravity : MonoBehaviour
 {
-    [SerializeField] GameObject PlanetDaddy;
+    public GameObject planetDaddy;
+    public float mass = 1;
 
-    private List<Planet> planets = new List<Planet>();
+    public GravityType gravityType;
+
+    private static List<Planet> planets = new List<Planet>();
     private Vector3 RandomVelocity;
 
     private void Awake()
     {
-        foreach (Planet planet in PlanetDaddy.GetComponentsInChildren<Planet>())
+        RandomVelocity = new Vector3(Random.Range(-20, 20), Random.Range(-20, 20), Random.Range(-20, 20));
+
+        if (planets.Count > 0) return;
+        foreach (Planet planet in planetDaddy.GetComponentsInChildren<Planet>())
         {
             planets.Add(planet);
         }
-
-        RandomVelocity = new Vector3(Random.Range(-20,20), Random.Range(-20,20), Random.Range(-20,20));
-    }
-
-
-    private void Start()
-    {
-        GetComponent<Rigidbody>().velocity = RandomVelocity;
     }
 
     private void FixedUpdate()
     {
-        Planet closest = GetClosestPlanet();
+        Vector3 gravityDirection = Vector3.zero;
 
-        Vector3 GravityDirection = (closest.transform.position - transform.position).normalized;
-        Physics.gravity = GravityDirection * Physics.gravity.magnitude;
+        switch (gravityType)
+        {
+            case GravityType.Closest:
+                Planet closest = GetClosestPlanet();
+                gravityDirection = closest.GravityDirection(transform.position, mass);
+                break;
+
+            case GravityType.All:
+                foreach(Planet planet in planets)
+                    gravityDirection += planet.GravityDirection(transform.position, mass);
+                break;
+        }
+
+        Physics.gravity = gravityDirection;
+        transform.LookAt(gravityDirection);
     }
 
     Planet GetClosestPlanet()
     {
         Planet closest = null;
         float closestDist = float.MaxValue;
-        foreach(Planet planet in planets)
+        foreach (Planet planet in planets)
         {
             float dist = Vector3.Distance(transform.position, planet.transform.position);
             if (dist < closestDist)
@@ -48,3 +61,29 @@ public class Gravity : MonoBehaviour
         return closest;
     }
 }
+
+[CustomEditor(typeof(Gravity))]
+public class GravityEditor : Editor
+{
+    SerializedProperty planetDaddy;
+    SerializedProperty mass;
+    SerializedProperty gravityType;
+
+    void OnEnable()
+    {
+        planetDaddy = serializedObject.FindProperty("planetDaddy");
+        mass = serializedObject.FindProperty("mass");
+        gravityType = serializedObject.FindProperty("gravityType");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        Gravity gravity = target as Gravity;
+
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(planetDaddy);
+        EditorGUILayout.PropertyField(mass);
+        EditorGUILayout.PropertyField(gravityType);
+        serializedObject.ApplyModifiedProperties();
+    }
+}   
--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -1,23 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Planet : MonoBehaviour
 {
-    [SerializeField] float GravityRadius = 15f;
-    void Start()
+    public bool useRadius = false;
+    [SerializeField] float radius = 10;
+    public bool useGravityScale = false;
+    [SerializeField] float gravityScale = 9.8f;
+    [SerializeField] float mass = 10;
+
+    public Vector3 GravityDirection(Vector3 from, float massF)
     {
-
-    }
-
-    void Update()
-    {
-
+        Vector3 GravityDirection = (transform.position - from).normalized;
+        float distance = Vector3.Distance(transform.position, from);
+        float GravityMagnitude = useGravityScale ? gravityScale : (massF * mass) / distance;
+        GravityDirection *= GravityMagnitude;
+        if (useRadius && distance > radius) return Vector3.zero;
+        return GravityDirection;
     }
 
     void OnDrawGizmos()
     {
+        if (!useRadius) return;
         Gizmos.color = Color.cyan;
-        //Gizmos.DrawWireSphere(transform.position, GravityRadius);
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
+
+[CustomEditor(typeof(Planet))]
+[CanEditMultipleObjects]
+public class PlanetEditor : Editor
+{
+    SerializedProperty useRadius;
+    SerializedProperty radius;
+    SerializedProperty useGravityScale;
+    SerializedProperty gravityScale;
+    SerializedProperty mass;
+    void OnEnable()
+    {
+        useRadius = serializedObject.FindProperty("useRadius");
+        radius = serializedObject.FindProperty("radius");
+        useGravityScale = serializedObject.FindProperty("useGravityScale");
+        gravityScale = serializedObject.FindProperty("gravityScale");
+        mass = serializedObject.FindProperty("mass");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        Planet planet = target as Planet;
+
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(useRadius);
+        if (planet.useRadius) EditorGUILayout.PropertyField(radius);
+        EditorGUILayout.PropertyField(useGravityScale);
+        if (planet.useGravityScale) EditorGUILayout.PropertyField(gravityScale);
+        if (!planet.useGravityScale) EditorGUILayout.PropertyField(mass);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}   
